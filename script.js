94d9995aca147a35fddf703bc6ec84c15a5ae79f
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-05 18:52:04 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,528 @@
+const MOVIES=[{
+    "name": "Who Is Harry Nilsson (And Why Is Everybody Talkin' About Him?)",
+    "genre": "Documentary",
+    "release_date": 2000,
+    "director": "Loveard"
+  }, {
+    "name": "Hoop Realities",
+    "genre": "Documentary",
+    "release_date": 1995,
+    "director": "Wilkison"
+  }, {
+    "name": "Voyeur (Abel)",
+    "genre": "Comedy",
+    "release_date": 2002,
+    "director": "Midden"
+  }, {
+    "name": "Infernal Affairs III (Mou gaan dou III: Jung gik mou gaan)",
+    "genre": "Action|Crime|Drama|Thriller",
+    "release_date": 1976,
+    "director": "Cattell"
+  }, {
+    "name": "Ella Lola, a la Trilby",
+    "genre": "(no genres listed)",
+    "release_date": 2012,
+    "director": "Springtorpe"
+  }, {
+    "name": "New Rulers of the World, The",
+    "genre": "Documentary",
+    "release_date": 1989,
+    "director": "Troyes"
+  }, {
+    "name": "Beverly Hills Cop III",
+    "genre": "Action|Comedy|Crime|Thriller",
+    "release_date": 1999,
+    "director": "Yeardley"
+  }, {
+    "name": "Road Hard",
+    "genre": "Comedy",
+    "release_date": 1998,
+    "director": "Lowres"
+  }, {
+    "name": "Leave It to Beaver",
+    "genre": "Comedy",
+    "release_date": 1992,
+    "director": "MacAulay"
+  }, {
+    "name": "Korso",
+    "genre": "Drama",
+    "release_date": 1968,
+    "director": "Yakunin"
+  }, {
+    "name": "Sliver",
+    "genre": "Thriller",
+    "release_date": 2003,
+    "director": "Beals"
+  }, {
+    "name": "Smiles of a Summer Night (Sommarnattens leende)",
+    "genre": "Comedy|Romance",
+    "release_date": 2000,
+    "director": "Grouse"
+  }, {
+    "name": "Midnight Meat Train, The",
+    "genre": "Horror|Mystery|Thriller",
+    "release_date": 2009,
+    "director": "Blackbrough"
+  }, {
+    "name": "Town Called Hell, A",
+    "genre": "Action|Comedy|Drama|Western",
+    "release_date": 1991,
+    "director": "Wolledge"
+  }, {
+    "name": "Humanité, L'",
+    "genre": "Crime|Drama",
+    "release_date": 2001,
+    "director": "Potten"
+  }, {
+    "name": "Beastmaster 2: Through the Portal of Time",
+    "genre": "Action|Adventure|Fantasy|Sci-Fi",
+    "release_date": 1990,
+    "director": "McMurray"
+  }, {
+    "name": "National Lampoon's Vacation",
+    "genre": "Comedy",
+    "release_date": 1967,
+    "director": "Baudino"
+  }, {
+    "name": "Battle Circus",
+    "genre": "Drama|Romance|War",
+    "release_date": 1966,
+    "director": "Fantham"
+  }, {
+    "name": "Kiss of the Vampire, The",
+    "genre": "Horror",
+    "release_date": 1995,
+    "director": "Torricella"
+  }, {
+    "name": "Cockfighter",
+    "genre": "Drama",
+    "release_date": 1997,
+    "director": "Trueman"
+  }, {
+    "name": "America's Most Haunted Inns",
+    "genre": "Documentary",
+    "release_date": 2008,
+    "director": "Brabin"
+  }, {
+    "name": "Shadrach",
+    "genre": "Drama",
+    "release_date": 2012,
+    "director": "Ruff"
+  }, {
+    "name": "Sittin' on a Backyard Fence",
+    "genre": "(no genres listed)",
+    "release_date": 2011,
+    "director": "Heinritz"
+  }, {
+    "name": "The Haunting of Helena",
+    "genre": "Horror|Thriller",
+    "release_date": 2009,
+    "director": "Glasheen"
+  }, {
+    "name": "Hollywood Sex Wars",
+    "genre": "Comedy",
+    "release_date": 2006,
+    "director": "Jaray"
+  }, {
+    "name": "Rink, The",
+    "genre": "Comedy",
+    "release_date": 2002,
+    "director": "Exelby"
+  }, {
+    "name": "Pink Saris",
+    "genre": "Documentary|Drama",
+    "release_date": 1994,
+    "director": "Ackred"
+  }, {
+    "name": "Whole",
+    "genre": "Documentary",
+    "release_date": 2012,
+    "director": "Robben"
+  }, {
+    "name": "Old Men in New Cars (Gamle mænd i nye biler)",
+    "genre": "Action|Comedy",
+    "release_date": 1997,
+    "director": "Leynton"
+  }, {
+    "name": "Amazing Spider-Man, The",
+    "genre": "Action|Adventure|Sci-Fi|IMAX",
+    "release_date": 2004,
+    "director": "Coverlyn"
+  }, {
+    "name": "The Fighting 69th",
+    "genre": "Action|Adventure|Drama|War",
+    "release_date": 2006,
+    "director": "Corris"
+  }, {
+    "name": "Greatest Story Ever Told, The",
+    "genre": "Drama",
+    "release_date": 2007,
+    "director": "Zamorano"
+  }, {
+    "name": "Lost Embrace (Abrazo partido, El)",
+    "genre": "Comedy|Drama",
+    "release_date": 1994,
+    "director": "Shearme"
+  }, {
+    "name": "Blue Kite, The (Lan feng zheng)",
+    "genre": "Drama",
+    "release_date": 2008,
+    "director": "Erlam"
+  }, {
+    "name": "Memories of Underdevelopment (Memorias del subdesarrollo)",
+    "genre": "Drama",
+    "release_date": 2011,
+    "director": "Manklow"
+  }, {
+    "name": "Baron Blood (Orrori del castello di Norimberga, Gli)",
+    "genre": "Horror",
+    "release_date": 2012,
+    "director": "Rapsey"
+  }, {
+    "name": "Unfaithful Wife, The (Femme infidèle, La)",
+    "genre": "Drama|Thriller",
+    "release_date": 1993,
+    "director": "Beale"
+  }, {
+    "name": "Life is to Whistle (Vida es silbar, La)",
+    "genre": "Drama",
+    "release_date": 1987,
+    "director": "Flintoff"
+  }, {
+    "name": "Bouncing Babies",
+    "genre": "Comedy",
+    "release_date": 1994,
+    "director": "Scoyles"
+  }, {
+    "name": "Griff the Invisible",
+    "genre": "Comedy|Drama|Romance",
+    "release_date": 2010,
+    "director": "Thornally"
+  }, {
+    "name": "Mysteries of Pittsburgh, The",
+    "genre": "Adventure|Comedy|Drama",
+    "release_date": 1996,
+    "director": "Rowlands"
+  }, {
+    "name": "Pirates! Band of Misfits, The",
+    "genre": "Adventure|Animation|Children|Comedy",
+    "release_date": 2002,
+    "director": "Barrs"
+  }, {
+    "name": "El Niño",
+    "genre": "Drama|Thriller",
+    "release_date": 2006,
+    "director": "Brecknock"
+  }, {
+    "name": "Amazing Grace and Chuck",
+    "genre": "Drama",
+    "release_date": 1995,
+    "director": "Calvard"
+  }, {
+    "name": "Prisoner of Shark Island, The",
+    "genre": "Drama",
+    "release_date": 1990,
+    "director": "Wittey"
+  }, {
+    "name": "Chosen, The",
+    "genre": "Drama",
+    "release_date": 2010,
+    "director": "Goodredge"
+  }, {
+    "name": "Beast from Haunted Cave",
+    "genre": "Crime|Horror|Romance",
+    "release_date": 1989,
+    "director": "Fatkin"
+  }, {
+    "name": "Nobody's Fool",
+    "genre": "Comedy|Romance",
+    "release_date": 1991,
+    "director": "Annakin"
+  }, {
+    "name": "Good Son, The",
+    "genre": "Drama|Thriller",
+    "release_date": 1999,
+    "director": "Broadberrie"
+  }, {
+    "name": "Running Time",
+    "genre": "Crime|Thriller",
+    "release_date": 2006,
+    "director": "Bleier"
+  }, {
+    "name": "Children Underground",
+    "genre": "Documentary",
+    "release_date": 2010,
+    "director": "Scothron"
+  }, {
+    "name": "Deep End, The",
+    "genre": "Drama",
+    "release_date": 1995,
+    "director": "Denty"
+  }, {
+    "name": "Jerichow",
+    "genre": "Drama",
+    "release_date": 2002,
+    "director": "Greedyer"
+  }, {
+    "name": "No Man's Land",
+    "genre": "Drama|War",
+    "release_date": 2007,
+    "director": "Greathead"
+  }, {
+    "name": "2-Headed Shark Attack",
+    "genre": "Comedy|Horror",
+    "release_date": 1999,
+    "director": "Szapiro"
+  }, {
+    "name": "Music for One Apartment and Six Drummers",
+    "genre": "Comedy",
+    "release_date": 1997,
+    "director": "Matisse"
+  }, {
+    "name": "Passing Fancy (Dekigokoro)",
+    "genre": "Drama",
+    "release_date": 1991,
+    "director": "Gerg"
+  }, {
+    "name": "Kanchenjungha",
+    "genre": "Drama",
+    "release_date": 2009,
+    "director": "Blackhall"
+  }, {
+    "name": "Mystery Team",
+    "genre": "Comedy|Crime|Mystery",
+    "release_date": 1994,
+    "director": "Heighway"
+  }, {
+    "name": "She Wouldn't Say Yes",
+    "genre": "Comedy|Romance",
+    "release_date": 2011,
+    "director": "Dentith"
+  }, {
+    "name": "Gamer",
+    "genre": "Action|Sci-Fi|Thriller",
+    "release_date": 1993,
+    "director": "Roarty"
+  }, {
+    "name": "Easy Life, The (Il Sorpasso)",
+    "genre": "Comedy|Drama",
+    "release_date": 2011,
+    "director": "Coots"
+  }, {
+    "name": "Little Giant, The",
+    "genre": "Comedy|Crime",
+    "release_date": 2008,
+    "director": "Croote"
+  }, {
+    "name": "Beautiful People",
+    "genre": "Comedy",
+    "release_date": 2009,
+    "director": "Schindler"
+  }, {
+    "name": "Unknown Known, The",
+    "genre": "Documentary",
+    "release_date": 2001,
+    "director": "Cornhill"
+  }, {
+    "name": "Trailer Park Boys: Live at the North Pole",
+    "genre": "Comedy",
+    "release_date": 1998,
+    "director": "Amorts"
+  }, {
+    "name": "Monsieur Lazhar",
+    "genre": "Children|Comedy|Drama",
+    "release_date": 2006,
+    "director": "Farrand"
+  }, {
+    "name": "Purple Gang, The",
+    "genre": "Crime|Drama",
+    "release_date": 2001,
+    "director": "Crowcombe"
+  }, {
+    "name": "Istanbul",
+    "genre": "(no genres listed)",
+    "release_date": 2011,
+    "director": "Nassy"
+  }, {
+    "name": "Bulli: Cooking in Progress, El",
+    "genre": "Documentary",
+    "release_date": 2011,
+    "director": "Crowche"
+  }, {
+    "name": "Book Thief, The",
+    "genre": "Children|Drama|War",
+    "release_date": 2010,
+    "director": "Goreway"
+  }, {
+    "name": "Ladrón que roba a ladrón",
+    "genre": "Action|Adventure|Comedy|Crime",
+    "release_date": 2000,
+    "director": "Kirlin"
+  }, {
+    "name": "It Started in Naples",
+    "genre": "Comedy|Drama",
+    "release_date": 2004,
+    "director": "Darnbrough"
+  }, {
+    "name": "Edvard Munch",
+    "genre": "Drama",
+    "release_date": 1996,
+    "director": "Godbolt"
+  }, {
+    "name": "Far from the Madding Crowd",
+    "genre": "Drama|Romance",
+    "release_date": 2001,
+    "director": "Boriston"
+  }, {
+    "name": "Kissed",
+    "genre": "Drama|Romance",
+    "release_date": 1992,
+    "director": "O'Cahey"
+  }, {
+    "name": "Mystery Date",
+    "genre": "Comedy",
+    "release_date": 2009,
+    "director": "Fullman"
+  }, {
+    "name": "Where East Is East",
+    "genre": "Drama",
+    "release_date": 1994,
+    "director": "Belsham"
+  }, {
+    "name": "California Dreamin' (Nesfarsit)",
+    "genre": "Comedy|Drama|War",
+    "release_date": 2006,
+    "director": "Entwhistle"
+  }, {
+    "name": "Johnny Belinda",
+    "genre": "Drama",
+    "release_date": 1966,
+    "director": "MacSkeagan"
+  }, {
+    "name": "Welcome to Woop-Woop",
+    "genre": "Comedy",
+    "release_date": 1999,
+    "director": "Comer"
+  }, {
+    "name": "Bad Blood (Mauvais sang)",
+    "genre": "Crime|Drama|Romance",
+    "release_date": 2007,
+    "director": "Goor"
+  }, {
+    "name": "Once in a Lifetime: The Extraordinary Story of the New York Cosmos",
+    "genre": "Documentary",
+    "release_date": 2010,
+    "director": "Antoniewicz"
+  }, {
+    "name": "Prelude to a Kiss",
+    "genre": "Comedy|Drama|Romance",
+    "release_date": 1995,
+    "director": "Grigson"
+  }, {
+    "name": "Fall of the Roman Empire, The",
+    "genre": "Drama",
+    "release_date": 2006,
+    "director": "MacDowal"
+  }, {
+    "name": "Wool Cap, The",
+    "genre": "Drama",
+    "release_date": 1995,
+    "director": "Taverner"
+  }, {
+    "name": "Brooklyn",
+    "genre": "Comedy|Drama|Romance",
+    "release_date": 2003,
+    "director": "Samwell"
+  }, {
+    "name": "Titanic",
+    "genre": "Action|Drama|Romance",
+    "release_date": 2009,
+    "director": "Worlock"
+  }, {
+    "name": "Waterloo",
+    "genre": "Action|Drama|War",
+    "release_date": 1995,
+    "director": "Asche"
+  }, {
+    "name": "The Trap",
+    "genre": "Crime|Mystery|Thriller",
+    "release_date": 1975,
+    "director": "Hassan"
+  }, {
+    "name": "Tyler Perry's Why Did I Get Married?",
+    "genre": "Comedy|Drama",
+    "release_date": 2001,
+    "director": "Vaan"
+  }, {
+    "name": "Nicholas and Alexandra",
+    "genre": "Drama|War",
+    "release_date": 1992,
+    "director": "Hrinchishin"
+  }, {
+    "name": "Gaslight",
+    "genre": "Drama|Thriller",
+    "release_date": 2007,
+    "director": "Folling"
+  }, {
+    "name": "The Mask You Live In",
+    "genre": "Documentary",
+    "release_date": 2009,
+    "director": "Colbron"
+  }, {
+    "name": "La nación clandestina",
+    "genre": "Drama",
+    "release_date": 1996,
+    "director": "Wilford"
+  }, {
+    "name": "Million to Juan, A",
+    "genre": "Comedy",
+    "release_date": 1997,
+    "director": "Dominguez"
+  }, {
+    "name": "Vares: The Path of the Righteous Men (Vares - Kaidan tien kulkijat)",
+    "genre": "Crime|Drama",
+    "release_date": 2008,
+    "director": "Knellen"
+  }, {
+    "name": "Enemy Mine",
+    "genre": "Adventure|Drama|Sci-Fi",
+    "release_date": 1996,
+    "director": "Reiling"
+  }, {
+    "name": "Angels Over Broadway",
+    "genre": "Adventure|Comedy|Crime|Drama",
+    "release_date": 1993,
+    "director": "Prestland"
+  }, {
+    "name": "Bikini Carwash Company, The",
+    "genre": "Comedy",
+    "release_date": 2010,
+    "director": "Fildes"
+  }]
+
+
+
+// create a database to search movies by title, genre, director, year
+
+let container = document.getElementById("movie-list-container")
+let movieDivs = MOVIES.map(function(movie){
+
+    let div= document.createElement("div")
+    div.innerText = movie.name
+    container.appendChild(div)
+    div.classList ="shown"
+    return div
+})
+
+document.addEventListener("keyup",function(e){
+    let text = e.target.value.toLowerCase()
+
+    movieDivs.forEach(function(movieDiv){
+        if(movieDiv.innerText.toLowerCase().includes 
+        (text)){
+            movieDiv.classList ="shown"
+        }else {
+            movieDiv.classList = "hidden"
+        }
+    })
+})
